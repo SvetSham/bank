a0f768ea7d1e7db45d71b4be740b3865253f8945
Светлана Шамбина <szxd2009@yandex.ru>
2025-07-16 15:52:21 +0300

    Добавлены логи во все функции.

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 """Вспомогательные функции, необходимые для работы функции страницы «Главная»"""
 import datetime
 import logging
-import sys
+# import sys
 import os
 import pandas as pd
 from config import BASE_DIR
@@ -10,7 +10,7 @@ import requests
 from dotenv import load_dotenv
 
 # Добавляем путь до корня проекта
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 logging.basicConfig(level=logging.DEBUG,
                     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -21,22 +21,22 @@ logging.basicConfig(level=logging.DEBUG,
 
 def greeting_by_time(moment: str) -> str:
     """Функция получает на вход дату в формате YYYY-MM-DD HH:MM:SS и возвращает соответствующее приветствие."""
-    prepare_date_logger=logging.getLogger('prepare_date')
+    greeting_by_time_logger = logging.getLogger('prepare_date')
     date_obj = datetime.datetime.strptime(moment, "%Y-%m-%d %H:%M:%S")
-    prepare_date_logger.info(f'Получена дата {date_obj.strftime("%d-%m-%Y %H:%M:%S")}')
+    greeting_by_time_logger.info(f'Получена дата {date_obj.strftime("%d-%m-%Y %H:%M:%S")}')
     greeting = ''
     if 3 <= date_obj.hour <= 8:
         greeting = "Доброе утро"
-        prepare_date_logger.info(f'Создано приветствие {greeting}')
+        greeting_by_time_logger.info(f'Создано приветствие {greeting}')
     elif 9 <= date_obj.hour <= 14:
         greeting = "Добрый день"
-        prepare_date_logger.info(f'Создано приветствие {greeting}')
+        greeting_by_time_logger.info(f'Создано приветствие {greeting}')
     elif 15 <= date_obj.hour <= 20:
         greeting = "Добрый вечер"
-        prepare_date_logger.info(f'Создано приветствие {greeting}')
+        greeting_by_time_logger.info(f'Создано приветствие {greeting}')
     else:
         greeting = "Доброй ночи"
-        prepare_date_logger.info(f'Создано приветствие {greeting}')
+        greeting_by_time_logger.info(f'Создано приветствие {greeting}')
     return greeting
 
 
@@ -59,22 +59,41 @@ def calculate_time_range(moment: str) -> tuple:
 
 def read_xlsx_file(path_to_file:str) -> pd.core.frame.DataFrame:
     """Функция читает xlsx-файл и возвращает DataFrame"""
+    read_xlsx_file_logger = logging.getLogger("read_xlsx_file")
+
     excel_data=pd.read_excel(path_to_file)
+
+    read_xlsx_file_logger.info(f'Открыт файл {path_to_file}.')
+
     return excel_data
 
 
 def str_to_date(date_str:str) -> datetime.datetime:
     """Функция преобразует входящую строку в объект datetime"""
-    return datetime.datetime.strptime(date_str, '%d.%m.%Y %H:%M:%S')
+    str_to_date_logger = logging.getLogger("str_to_date")
+
+    date_result = datetime.datetime.strptime(date_str, '%d.%m.%Y %H:%M:%S')
+
+    str_to_date_logger.info(f'Преобразована из строки дата {date_result}.')
+
+    return date_result
 
 
 def filter_by_date(excel_data:pd.core.frame.DataFrame, moment:str)->pd.core.frame.DataFrame:
     """Функция принимает на вход датафрейм с транзакциями и дату. На выходе выдает
     датафрейм с транзакциями, прошедшими от начала месяца до введённой даты. """
-    date_start, date_stop=calculate_time_range(moment)
+    filter_by_date_logger = logging.getLogger('filter_by_date')
+
+    date_start, date_stop = calculate_time_range(moment)
+    filter_by_date_logger.info(f"Создан диапазон дат.")
+
     excel_data['Дата операции'] = excel_data['Дата операции'].apply(str_to_date)
+    filter_by_date_logger.info("Все даты преобразованы из строки в объект datetime.")
+
     filtered_data=excel_data.loc[(excel_data['Дата операции'] >=date_start)
                                  & (excel_data['Дата операции'] <= date_stop)]
+    filter_by_date_logger.info("Отфильтрованы данные по транзакциям для данного диапазона дат.")
+
     return filtered_data
 
 
@@ -82,33 +101,48 @@ def get_last_four_numbers_of_card(card_full_number:str) -> str:
     """Функция принимает номер карты в виде строки и,
     если это правда строка, то возвращает последние 4 цифры
     номера карты в виде строки. Иначе возвращает пустую строку. """
+    get_last_four_numbers_of_card_logger = logging.getLogger("get_last_four_numbers_of_card")
     if isinstance(card_full_number, str):
-        return card_full_number[-4:]
+        card_short_number = card_full_number[-4:]
+        get_last_four_numbers_of_card_logger.info(f"Номер карты представлен в виде последних четырех цифр {card_short_number}.")
+        return card_short_number
     else:
+        get_last_four_numbers_of_card_logger.info("Транзакция проходила по счёту, а не по карте.")
         return ''
 
 
 def cut_card_number(cutted_data:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     """Функция принимает датафрейм и возвращает датафрейм с номерами карт в виде последних четырёх цифр."""
+    cut_card_number_logger = logging.getLogger("cut_card_number")
+
     cutted_data.loc[:,'Номер карты'] = cutted_data['Номер карты'].apply(get_last_four_numbers_of_card)
+    cut_card_number_logger.info("Все номера карт приведены к последним четырем цифрам.")
+
     return cutted_data
 
 
 def make_grouped(grouped_data:pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     """ Функция группирует данные по номерам карт"""
+    make_grouped_logger = logging.getLogger('make_grouped')
     grouped_data = grouped_data.groupby("Номер карты")
+    make_grouped_logger.info("Транзакции сгруппированы по номерам карт.")
     return grouped_data
 
 
 def read_user_settings_file(path_to_file:str) -> dict:
     """ Функция читает JSON-файл с пользовательскими настройками и возвращает словарь"""
+    read_user_settings_file_logger = logging.getLogger("read_user_settings_file")
     with open(path_to_file) as file:
         user_settings = json.load(file)
+    read_user_settings_file_logger.info(f"Открыт файл {path_to_file}.")
     return user_settings
 
 def find_exchange_rate(currency:str) -> str:
+    """ Функция ищет, сколько рублей сегодня стоит валюта, указанная в user_settings"""
+    find_exchange_rate_logger = logging.getLogger("find_exchange_rate")
     load_dotenv()
     API_KEY_CUR = os.getenv("API_KEY_CUR")
+    find_exchange_rate_logger.info("Получен API_KEY для поиска курса валют.")
     base = currency
     symbols = 'RUB'
     url = "https://api.apilayer.com/exchangerates_data/latest"
@@ -116,24 +150,36 @@ def find_exchange_rate(currency:str) -> str:
     payload = {"base":base, "symbols":symbols}
     headers = {"apikey": API_KEY_CUR}
 
+    find_exchange_rate_logger.info(f"Отправлен запрос по адресу {url}, узнать, сколько рублей стоит {base}.")
     response = requests.request("GET", url, headers=headers, params=payload)
+
     status_code = response.status_code
+    find_exchange_rate_logger.info(f'Получен код статуса {status_code}.')
     result = response.text
+
     rate = round(response.json()["rates"][symbols], 2)
+    find_exchange_rate_logger.info(f'Получен курс: 1 {base} = {rate} RUB.')
+
     return rate
 
 
 def find_stock_price(stock:str) -> float:
+    """ Функция ищет, сколько рублей сегодня на время открытия биржи стоят акции, указанные в user_settings"""
+    find_stock_price_logger = logging.getLogger("find_stock_price")
     load_dotenv()
     API_KEY_STOCK = os.getenv("API_KEY_STOCK")
+    find_stock_price_logger.info("Получен API_KEY для поиска стоимости акций.")
+
     function = "GLOBAL_QUOTE"
     symbol = stock
-    headers = {"apikey": API_KEY_STOCK}
-    payload = {"function": function, "symbol": symbol}
     url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&apikey={API_KEY_STOCK}'
+    find_stock_price_logger.info(f"Отправлен запрос по адресу {url}, узнать, сколько рублей стоит акция {symbol}.")
     response = requests.get(url)
     status_code = response.status_code
+    find_stock_price_logger.info(f'Получен код статуса {status_code}.')
     result = response.text
+
     stock_price = round(float(response.json()["Global Quote"]["02. open"]), 2)
+    find_stock_price_logger.info(f'Получена стоимость акции {symbol} = {stock_price} RUB.')
     return stock_price
 
--- src/views.py
+++ src/views.py
@@ -1,31 +1,37 @@
 """ Основные функции для генерации JSON-ответов """
 import utils
-import datetime
 from config import BASE_DIR
+import logging
 
 
 def make_main_content(date:str) -> dict:
+    """ Функция формирует словарь с данными по картам, транзакциям
+    за диапазон от начала месяца до указанной даты включительно, а также
+    по текущим курсам валют и стоимости акций, указанными в user_settings"""
+
+    make_main_content_logger = logging.getLogger("make_main_content")
+    make_main_content_logger.info("Начинаем сбор данных для формирования главной страницы.")
     result = {"greeting" : utils.greeting_by_time(date)}
-    # Подготавливаем данные.
 
-    # Открываем xlsx-файл.
     data = utils.read_xlsx_file(BASE_DIR / "data" / "operations.xlsx")
+    make_main_content_logger.info("Открыт xlsx-файл с транзакциями.")
 
-    # Берём транзакции в заданном диапазоне.
     filtered_by_date = utils.filter_by_date(data, date)
+    make_main_content_logger.info("Взяты данные только для заданного диапазона дат.")
 
-    # Преобразуем номер карты в короткий - последние 4 цифры номера карты.
     cutted_data = utils.cut_card_number(filtered_by_date)
+    make_main_content_logger.info("Номер карты преобразован в короткий - последние 4 цифры номера карты.")
 
-    # Группировка по номеру карты.
     grouped = utils.make_grouped(cutted_data)
+    make_main_content_logger.info("Данные сруппированы по номеру карты.")
 
-    # Получаем информацию о картах и транзакциях.
+    make_main_content_logger.info("Готовим информацию о картах и транзакциях.")
     top_transactions = []
     cards_list = []
     for name, group in grouped:
         if name == "": # Это транзакции. Сортируем транзакции по убыванию суммы платежа.
             group.sort_values(by='Сумма платежа', axis=0, ascending=False, inplace=True)
+            make_main_content_logger.info("Транзакции по счетам отсортированы по убыванию суммы платежа")
             if group['Сумма платежа'].count() >= 5:
                 for i in range(5):
                     transaction_info = {"date": group.iloc[i, 1],
@@ -44,6 +50,7 @@ def make_main_content(date:str) -> dict:
                                         }
                     top_transactions.append(transaction_info)
                     i += 1
+            make_main_content_logger.info("Получили информацию о топ-5 транзакциях по счетам по сумме платежа.")
         else: # Это карта. Собираем информацию по каждой карте.
             total_spent = 0
             for i in range(group["Сумма платежа"].count()):
@@ -58,13 +65,16 @@ def make_main_content(date:str) -> dict:
                 "cashback": cashback
             }
             cards_list.append(card_info)
+            make_main_content_logger.info(f"Получена информация по карте {name}.")
 
     result["cards"] = cards_list
     result["top_transactions"] = top_transactions
 
     # Открываем JSON-файл с пользовательскими настройками.
     user_settings = utils.read_user_settings_file(BASE_DIR / "user_settings.json")
+    make_main_content_logger.info("Открыт файл user_settings.")
 
+    # Ищем курсы валют на сегодня
     currency_rate = []
     for currency in user_settings["user_currencies"]:
         current_currency = {
@@ -74,7 +84,9 @@ def make_main_content(date:str) -> dict:
         currency_rate.append(current_currency)
 
     result["currency_rates"] = currency_rate
+    make_main_content_logger.info("Для каждой интересующей пользователя валюте получили курс на сегодняшний день.")
 
+    # Ищем стоимость акций на сегодня
     stock_prices = []
     for stock in user_settings['user_stocks']:
         current_stock = {
@@ -84,8 +96,9 @@ def make_main_content(date:str) -> dict:
         stock_prices.append(current_stock)
 
     result["stock_prices"] = stock_prices
+    make_main_content_logger.info("Для каждой акции получили стоимость на открытие биржи сегодня.")
 
     return result
 
 
-#print(make_main_content("2018-07-30 13:40:31"))
+print(make_main_content("2018-07-30 13:40:31"))
